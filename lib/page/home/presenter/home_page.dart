08cf2f272e1fc5733a03e707103530c4ce3a33d0
Rodrigo Bastos <rodrigobastosv@gmail.com>
2022-04-20 15:30:41 -0300

    adjusts

--- lib/page/home/presenter/home_page.dart
+++ lib/page/home/presenter/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:expandable_fab_menu/expandable_fab_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jobsity_chalenge/core/core.dart';
@@ -80,6 +81,46 @@ class HomePage extends StatelessWidget {
               separatorBuilder: (_, i) => const SizedBox(height: 8),
               itemCount: state.shows.length,
             ),
+          ),
+          floatingActionButton: ExpandableFabMenu(
+            onPress: () {},
+            animatedIcon: AnimatedIcons.menu_home,
+            animatedIconTheme: const IconThemeData(size: 22.0),
+            child: const Icon(Icons.add),
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            onOpen: () => debugPrint('OPENING DIAL'),
+            onClose: () => debugPrint('DIAL CLOSED'),
+            overlayColor: Colors.black,
+            overlayOpacity: 0.7,
+            children: [
+              ExpandableFabMenuItem(
+                child: const Icon(Icons.favorite, color: Colors.white),
+                title: 'Favorite Shows',
+                titleColor: Colors.white,
+                subtitle: 'Manage your favorite shows',
+                subTitleColor: Colors.white,
+                backgroundColor: Colors.amber,
+                onTap: () => debugPrint('FIRST CHILD'),
+              ),
+              ExpandableFabMenuItem(
+                child: const Icon(Icons.visibility, color: Colors.white),
+                title: 'People Search',
+                titleColor: Colors.white,
+                subtitle: 'Find people you want to know more about',
+                subTitleColor: Colors.white,
+                backgroundColor: Colors.green,
+                onTap: () => debugPrint('SECOND CHILD'),
+              ),
+              ExpandableFabMenuItem(
+                child: const Icon(Icons.settings, color: Colors.white),
+                title: 'Settings',
+                titleColor: Colors.white,
+                subtitle: 'Adjust the settings of the app',
+                subTitleColor: Colors.white,
+                backgroundColor: Colors.blue,
+                onTap: () => debugPrint('THIRD CHILD'),
+              ),
+            ],
           ),
         );
       },
